fe1915ace384f5208559b3f6b190d36117a01d58
A K M Mahdi <94472254+AKM-Mahdi@users.noreply.github.com>
2022-12-10 22:47:08 +0600

    header_fixed

--- src/COMPONENTS/Footer/Footer.jsx
+++ src/COMPONENTS/Footer/Footer.jsx
@@ -1,7 +1,62 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      {/* <>
+        <div className="headerSection container-fluid ">
+          <div className="left col-xl-1 col-lg-2 col-md-2 ">
+            <Link to="/">
+              <img width={90} src={logo} alt="" />
+            </Link>
+          </div>
+
+          <div className="col-xl-8 col-lg-4 col-md-3 ">
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Find products "
+                aria-label="Search"
+              />
+              <Button className="searchBtn">
+                <BiSearch></BiSearch>
+              </Button>
+            </Form>
+          </div>
+
+          <div className="right col-xl-3 col-lg-6 col-sm-4 col-md-7  d-md-block d-none">
+            <Nav className="justify-content-end ">
+              <Link to="/signin">
+                <small className="">Deliver to</small>
+                <br />
+                <p>
+                  <BiMap></BiMap> Bangladesh
+                </p>
+              </Link>
+              <Link to="/signin">
+                <small className="">Hello, sign in</small>
+                <br />
+                <p>Account $ Lists</p>
+              </Link>
+              <Link to="/signin">
+                <small>Returns</small>
+                <br />
+                <p>& Orders</p>
+              </Link>
+              <Link className="nav-link" to="/signin">
+                <BsBasket2Fill></BsBasket2Fill>
+              </Link>
+            </Nav>
+          </div>
+          <div className="mobileMenu d-md-none text-light ms-5">
+            <button className="mobileMenu">
+              <BiMenu className="mobileMenu"></BiMenu>
+            </button>
+          </div>
+        </div>
+      </> */}
+    </div>
+  );
 };
 
 export default Footer;
--- src/COMPONENTS/Header/Header.jsx
+++ src/COMPONENTS/Header/Header.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
 
 import { Link } from "react-router-dom";
 import logo from "../../ASSETS/SITE_IDENTITY/logo.png";
@@ -11,54 +8,34 @@ import { BiSearch, BiMenu, BiMap } from "react-icons/bi";
 import "./Header.css";
 const Header = () => {
   return (
-    <div className="headerSection container-fluid ">
-      <div className="left col-xl-1 col-lg-2 col-md-2 ">
-        <Link to="/">
-          <img width={90} src={logo} alt="" />
+    <div className="headerSection container-fluid">
+      <img className="logo" src={logo} alt="" />
+      <div className="headerSearch">
+        <input type="text" className="headerSearchInput" />
+        <BiSearch className="headerSearchBtn" />
+      </div>
+      <div className="navMenu">
+        <Link className="nav-link" to="/signin">
+          <span className="">Deliver to</span>
+          <br />
+          <p>
+            <BiMap></BiMap> Bangladesh
+          </p>
         </Link>
-      </div>
-
-      <div className="col-xl-8 col-lg-6 col-md-5 ">
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Find products "
-            aria-label="Search"
-          />
-          <Button className="searchBtn">
-            <BiSearch></BiSearch>
-          </Button>
-        </Form>
-      </div>
-
-      <div className="right col-xl-3 col-lg-4 col-sm-4 col-md-5  d-md-block d-none">
-        <Nav className="justify-content-end ">
-          <Link to="/signin">
-            <small className="">Deliver to</small>
-            <br />
-            <p>
-              <BiMap></BiMap> Bangladesh
-            </p>
-          </Link>
-          <Link to="/signin">
-            <small className="">Hello, sign in</small>
-            <br />
-            <p>Account $ Lists</p>
-          </Link>
-          <Link to="/signin">
-            <small>Returns</small>
-            <br />
-            <p>& Orders</p>
-          </Link>
-          <Link className="nav-link" to="/signin">
-            <BsBasket2Fill></BsBasket2Fill>
-          </Link>
-        </Nav>
-      </div>
-      <div className="mobileMenu d-md-none text-light ms-5">
-        <button className="mobileMenu">
-          <BiMenu className="mobileMenu"></BiMenu>
-        </button>
+        <Link to="/signin">
+          <span className="">Hello, sign in</span>
+          <br />
+          <p>Account $ Lists</p>
+        </Link>
+        <Link to="/signin">
+          <span>Returns</span>
+          <br />
+          <p>& Orders</p>
+        </Link>
+        <Link className="nav-link" to="/signin">
+          <BsBasket2Fill></BsBasket2Fill>
+          <span className="cartCount">0</span>
+        </Link>
       </div>
     </div>
   );
